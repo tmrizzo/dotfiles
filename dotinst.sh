9a775c284f4215ee893df1dba60cce3e3fe3d921
tmrizzo <tiagomdrizzo@gmail.com>
2018-07-20 21:50:06 -0300

    Added dotinst.sh

---
+++ dotinst.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+ln -vsf ~/git/dotfiles/fonts/ ~/.fonts
+ln -vsf ~/git/dotfiles/terminator/ ~/.config/terminator
+ln -vsf ~/git/dotfiles/weechat/ ~/.config/weechat
+ln -vsf ~/git/dotfiles/tmux/tmux.conf ~/.tmux
+ln -vsf ~/git/dotfiles/README.md
+ln -vsf ~/git/dotfiles/LICENSE
+ln -vsf ~/git/dotfiles/bashrc.d/ ~/.bashrc.d
+ln -vsf ~/git/dotfiles/bash_profile ~/.bash_profile
+ln -vsf ~/git/dotfiles/bashrc ~/.bashrc
+ln -vsf ~/git/dotfiles/vim/ ~/.vim
+ln -vsf ~/git/dotfiles/vimrc ~/.vimrc
+ln -vsf ~/git/dotfiles/Xresources ~/.Xresources
+ln -vsf ~/git/dotfiles/gitconfig ~/.gitconfig
